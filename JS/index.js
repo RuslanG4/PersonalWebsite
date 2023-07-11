2a9b851ff634a75921e6a6d7590fd112995bc645
“Ruslan <ruslangavrilov118@gmail.com>
2023-07-11 18:21:16 +0100

    i keep changing this site

--- JS/index.js
+++ JS/index.js
@@ -7,3 +7,17 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
 
+  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style. backgroundColor = "#141414";
+  } else {
+    document.getElementById("navbar").style.backgroundColor = "transparent";
+  }
+}
+  function dropDown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+
