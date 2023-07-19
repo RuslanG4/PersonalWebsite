1e56b3ae96297e4250ed8a0ead083ec262cfe787
Full Name <ruslangavrilov118@gmail.com>
2023-07-19 16:53:49 +0100

    added paragraph appearance js

---
+++ WEBPAGES/Year2Page/JS/TextAppear.js
@@ -0,0 +1,36 @@
+const checkpoint = 600;
+  
+  function reveal() {
+      var reveals = document.querySelectorAll(".text");
+  
+      const currentScroll = window.scrollY;
+  
+      if(checkpoint>=currentScroll)
+      {
+  
+        for (var i = 0; i < reveals.length; i++) {
+          var windowHeight = window.innerHeight;
+          var elementTop = reveals[i].getBoundingClientRect().top;
+          var elementVisible = 150;
+        
+          if (elementTop < windowHeight - elementVisible) {
+            reveals[i].style.color = "aliceblue"
+          } else {
+            reveals[i].style.color = "transparent"
+          }
+        }
+      }
+      else {
+        for (var i = 0; i < reveals.length; i++) {
+        reveals[i].style.color = "aliceblue"
+      }
+      }
+}
+
+
+function update(){
+    reveal();
+    window.requestAnimationFrame(update);
+  }
+  
+  window.requestAnimationFrame(update);
